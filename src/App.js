87191563ed67740966d5e740ad2b77d4c4b33fbe
IrumiDesu7 <ikalam89@gmail.com>
2022-06-26 19:47:32 +0800

    add logic to populate display

--- src/App.js
+++ src/App.js
@@ -1,30 +1,84 @@
+import { useReducer } from 'react';
+import DigitButton from './components/DigitButton';
+import OperationButton from './components/OperationButton';
 import './styles.css';
 
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  BACKSPACE: 'backspace',
+  CHOOSE_OPERATION: 'choose-operation',
+  EVALUATE: 'evaluate',
+};
+
+const reducer = (state, { type, payload }) => {
+  // eslint-disable-next-line default-case
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === '0' && state.firstOperand === '0') {
+        return state;
+      }
+      if (payload.digit === '.' && state.firstOperand.includes('.')) {
+        return state;
+      }
+
+      if (state.operation != null) {
+        return {
+          ...state,
+          secondOperand: `${state.secondOperand || ''}${payload.digit}`,
+        };
+      }
+
+      return {
+        ...state,
+        firstOperand: `${state.firstOperand || ''}${payload.digit}`,
+      };
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.firstOperand == null) {
+        return state;
+      }
+      if (state.secondOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+    // eslint-disable-next-line no-fallthrough
+    case ACTIONS.CLEAR:
+      return {};
+  }
+};
+
 function App() {
+  const [{ firstOperand, secondOperand, operation, result }, dispatch] =
+    useReducer(reducer, {});
   return (
     <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'>1231</div>
-        <div className='current-operand'>1231</div>
+        <div className='operation'>
+          {firstOperand} {operation} {secondOperand}
+        </div>
+        <div className='result'>{result}</div>
       </div>
-      <button>AC</button>
+      <button onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
       <button>( )</button>
       <button>%</button>
-      <button>÷</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>x</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>-</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-      <button>0</button>
-      <button>.</button>
+      <OperationButton operation='÷' dispatch={dispatch} />
+      <DigitButton digit='7' dispatch={dispatch} />
+      <DigitButton digit='8' dispatch={dispatch} />
+      <DigitButton digit='9' dispatch={dispatch} />
+      <OperationButton operation='x' dispatch={dispatch} />
+      <DigitButton digit='4' dispatch={dispatch} />
+      <DigitButton digit='5' dispatch={dispatch} />
+      <DigitButton digit='6' dispatch={dispatch} />
+      <OperationButton operation='-' dispatch={dispatch} />
+      <DigitButton digit='1' dispatch={dispatch} />
+      <DigitButton digit='2' dispatch={dispatch} />
+      <DigitButton digit='3' dispatch={dispatch} />
+      <OperationButton operation='+' dispatch={dispatch} />
+      <DigitButton digit='0' dispatch={dispatch} />
+      <DigitButton digit='.' dispatch={dispatch} />
       <button>⌫</button>
       <button>=</button>
     </div>
